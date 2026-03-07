174e0fc367f32e8c397d03fc62f0f881c116200a
charannyk06 <charannyan@gmail.com>
2026-03-07 17:25:00 -0500

    fix cli release packaging

---
+++ scripts/pack-cli-release.mjs
@@ -0,0 +1,34 @@
+import { packCliReleasePackage } from "./cli-release-stage.mjs";
+
+function parseArgs(argv) {
+  const options = {};
+
+  for (let index = 0; index < argv.length; index += 1) {
+    const arg = argv[index];
+
+    if (arg === "--pack-destination") {
+      options.packDestination = argv[index + 1];
+      index += 1;
+      continue;
+    }
+
+    if (arg === "--stage-dir") {
+      options.stageDir = argv[index + 1];
+      index += 1;
+      continue;
+    }
+
+    if (arg === "--root-dir") {
+      options.rootDir = argv[index + 1];
+      index += 1;
+      continue;
+    }
+
+    throw new Error(`Unknown argument: ${arg}`);
+  }
+
+  return options;
+}
+
+const { tarballPath } = packCliReleasePackage(parseArgs(process.argv.slice(2)));
+console.log(tarballPath);
